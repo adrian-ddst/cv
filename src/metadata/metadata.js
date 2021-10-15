cc6f600c05cab6ac2d318a483c3d5bcb89d54a20
deadset47 <adriantepsan@gmail.com>
2021-10-15 21:58:59 +0300

    all other stuff

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -11,40 +11,20 @@ module.exports = {
     {
       title: 'Frontend Developer - mindit.io',
       written: 'Aug 2021 - Present',
-      badges: ['mindit.io', 'Frontend', 'Redux', 'OpenAPI'],
+      badges: ['mindit.io', 'Frontend', 'Pharma', 'Redux', 'OpenAPI'],
       contents: `At mindit.io, my most recent role has been as a Frontend Developer in a brand-new project which started a few days before my arrival. In the beginning, there was a major need in developing the architecture and starting the development of the interface as soon as possible. In this case, it was about an application in the pharmaceutical domain, and the client was in a haste to see a palpable result. We used Angular as the frontend framework, accompanied by some nice adjacent technologies to ease our work, such as Redux and OpenAPI. The backend application was written initially in Spring, but currently migrating to .NET because we decided that it will fit better for a future integration with other Microsoft services. I've worked in a team of professionals with whom I related very well and our teamwork factor was beyond the limits.`
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: 'Frontend Developer - mindit.io',
+      written: 'Jul 2020 - Aug 2021',
+      badges: ['mindit.io', 'Frontend', 'Backend', 'Lead'],
+      contents: `In the summer of 2020 I was assigned to work on an on-going project as a Frontend Developer. The project incorporated a quantities management application for a large consuming goods company. The frontend was designed in Angular, and used a lot of utility JavaScript libraries such as RxJS, Lodash, etc. In this project, I've also contributed to the backend side of the project, as it was written in Spring. It was easier for me to learn Spring by solving tasks and experiencing the backend hands-on. Later on, after a couple of months have passed, I was assigned as the frontend lead on the project due to my proven interest and hard work. I was in charge of deciding design patterns, planning on new implementations, discussions with the client and estimates for every functionality. The team was great, the most friendly I've ever met. We were first of all friends, then colleagues, and I also had the chance to be the "buddy" for new colleagues that needed to get aquainted with the project or to help them learn a specific technology.`
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'Frontend Developer - mindit.io',
+      written: 'Jul 2019 - Jul 2020',
+      badges: ['mindit.io', 'Internship', 'Angular'],
+      contents: `In 2019 I started my career as a Software Developer at mindit.io. I went through a 3-month internship program, during which I worked among other experienced developers who were willing to help me grow. During the internship, I became interested in Frontend Development and I started investing a lot of interest in learning about this amazing part of Web Development. After the internship period has ended, mindit.io offered me to continue the collaboration and assigned me to an on-going project in the company. In this project, I was designated as a Frontend Developer and I worked in a big team, composed mostly of the people to whom I was close also during the internship. It was nice to be already familiar with the people. As for the project, it was about sales of consuming goods, where users would be able to publish their products with the corresponding price, in order to be validated by various commissions. On this project there was a big need of frontend development, considering the multitude of functionalities needed to accomplish such objectives.`
     }
   ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ]
 };
